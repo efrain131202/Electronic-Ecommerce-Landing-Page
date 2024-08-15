306b57a9337d9b359be2b1a59bebf1ae68e45184
Efrain131202 <efraincruzlobato13@gmail.com>
2024-08-15 14:52:05 -0600

    haciendo responsivo Features y creando slider

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Features() {
     return (
-        <main className='justify-center mt-[100px]'>
+        <main className='justify-center mt-[50px]'>
             <div className='text-center'>
-                <h1 className='text-black text-[74px] font-extrabold leading-[92.50px]'>What <span className=' text-[#5e1ee5] font-extrabold leading-[92.50px]'>we</span> provide?</h1>
+                <h1 className='text-black text-[48px] sm:text-[74px] font-extrabold leading-[58px] sm:leading-[92.50px]'>
+                    What <span className=' text-[#5e1ee5] font-extrabold leading-[58px] sm:leading-[92.50px]'>we</span> provide?
+                </h1>
             </div>
-            <div className='flex flex-wrap text-center items-center justify-center overflow-hidden relative p-[10px] gap-[100px] mt-[50px]'>
+            <div className='flex flex-nowrap sm:flex-wrap text-center items-center justify-center overflow-x-auto sm:overflow-hidden relative p-[10px] gap-[10px] sm:gap-[50px] md:gap-[100px] mt-[30px] sm:mt-[50px]'>
                 {[
                     { src: "/frames/Frame.png", text: ["Computer &", <br/>, "Laptop"] },
                     { src: "/frames/Frame (1).png", text: ["Mobile &", <br/>, "Phone"] },
@@ -16,11 +18,11 @@ function Features() {
                     { src: "/frames/Frame (5).png", text: "Accessories" },
                     { src: "/frames/Frame (6).png", text: ["Other", <br/>, "Categories"] }
                 ].map((item, index) => (
-                    <div key={index} className='flex flex-col items-center'>
-                        <div className='w-[85px] h-[85px] bg-white rounded-full shadow-lg flex items-center justify-center'>
-                            <img className='w-10 h-10 object-contain' src={item.src} alt="" />
+                    <div key={index} className='flex flex-col items-center min-w-[80px] sm:min-w-0'>
+                        <div className='w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] md:w-[85px] md:h-[85px] bg-white rounded-full shadow-lg flex items-center justify-center'>
+                            <img className='w-5 h-5 sm:w-6 sm:h-6 md:w-10 md:h-10 object-contain' src={item.src} alt="" />
                         </div>
-                        <p className='text-lg font-medium leading-normal mt-4 text-center'>
+                        <p className='hidden sm:block text-center text-xs sm:text-lg font-medium leading-normal mt-2 sm:mt-4'>
                             {item.text}
                         </p>
                     </div>
